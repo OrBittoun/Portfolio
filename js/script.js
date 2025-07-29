7e81534a84602f6e1b2f6e8b8ae3610c0916d222
Or Bittoun <orbitzx25@gmail.com>
2025-07-30 00:35:51 +0300

    Finished Project

--- js/script.js
+++ js/script.js
@@ -2,3 +2,29 @@ function showSuccessMessage(){
     alert("Message on its way!")
     return true;
 }
+
+
+const modebtn = document.getElementById("toggleModeBtn");
+const modeIcon = document.getElementById("modeIcon");
+
+modebtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    modeIcon.classList.remove("bi-moon");
+    modeIcon.classList.add("bi-sun");
+
+    modebtn.classList.remove("btn-dark");
+    modebtn.classList.add("btn-light");
+
+  } 
+  
+  else {
+    modeIcon.classList.remove("bi-sun");
+    modeIcon.classList.add("bi-moon");
+
+    modebtn.classList.remove("btn-light");
+    modebtn.classList.add("btn-dark");
+  }
+});
+
